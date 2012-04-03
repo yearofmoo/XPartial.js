896021f4bc08f0f4a15992fd8585e74e36d5092c
Matias Niemela <matias@yearofmoo.com>
2012-04-03 12:46:19 -0400

    Scour script updated

--- Source/XPartial.Scour.js
+++ Source/XPartial.Scour.js
@@ -2,10 +2,13 @@ Scour.Global.defineRole('XPartial',function(element,options) {
 
   var partial;
   var href = element.get('href');
+  var inject = options.get('target');
+  var target = inject ? 'inside' : 'after';
 
   element.addEvent('click',function(event) {
     event.stop();
     partial = new XPartial;
+    $(partial).inject(inject || element,target);
     partial.load(href);
   });
 
